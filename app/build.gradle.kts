1f38e8543eca7d1b7c0f732faa779ea9ee5c63f7
MaxMariusJacobs <max.m.jacobs@stud.leuphana.de>
2026-09-24 19:00:08 +0200

    finalization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.example.app_blindenstock_add_on"
         minSdk = 34
         targetSdk = 35
-        versionCode = 11
-        versionName = "2.2"
+        versionCode = 12
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
